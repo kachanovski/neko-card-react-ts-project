3bfa648b98a74189938d7daae5c03d6f340b70ac
kachanovski <58528758+kachanovski@users.noreply.github.com>
2020-09-23 17:36:47 +0300

    Updates

---
+++ precache-manifest.27fedfceefb3375e72a9f0b2e5574137.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e2929b502da2b5b3e08d7d2b1069b75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d26f216682edc4191510",
+    "url": "/static/css/main.f0c9c1a0.chunk.css"
+  },
+  {
+    "revision": "4698ee72da9bb03487bb",
+    "url": "/static/js/2.f69ac783.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f69ac783.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d26f216682edc4191510",
+    "url": "/static/js/main.df0f0022.chunk.js"
+  },
+  {
+    "revision": "4a9f2025771dffa17540",
+    "url": "/static/js/runtime-main.db9c1256.js"
+  },
+  {
+    "revision": "9ad033144d39ff7ef310a4046484b7dd",
+    "url": "/static/media/background_page_login.9ad03314.jpg"
+  }
+]);
