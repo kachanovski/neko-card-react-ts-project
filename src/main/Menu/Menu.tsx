c3bf30d611d9d9e1500c9c587a48b07d274636e3
Artem <rozen19942810@gmail.com>
2020-10-21 16:07:26 +0300

    add Preloader in LoginForm.tsx, refactor LoginReducer.ts, Menu.tsx

--- src/main/Menu/Menu.tsx
+++ src/main/Menu/Menu.tsx
@@ -1,48 +1,69 @@
-import React, {useState} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import {NavLink} from "react-router-dom";
 import s from './Menu.module.scss'
 import {useDispatch, useSelector} from "react-redux";
-import {logOutAction, setLogOutUser} from "../../store/LoginReducer";
+import {AuthMe, setLogOutUser} from "../../store/LoginReducer";
 import {StateType} from "../../store/redux-store";
 
-const Menu = () => {
+const Menu = React.memo (() => {
 
-    const [isHide, setIsHide] = useState(false)
+    const [isHide, setIsHide] = useState(true)
     const authMe = useSelector<StateType, boolean>(state => state.login.authMe)
     const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(AuthMe())
+    }, [])
+
     const logOut = () => {
-        dispatch(logOutAction())
         dispatch(setLogOutUser())
     }
-
-    const showMenu = () => {
-        setIsHide(false)
-    }
-    const hideMenu = () => {
-        setIsHide(true)
-    }
+    const showHideMenuTrigger = useCallback(() => {
+        if (isHide) setIsHide(false)
+        if (!isHide) setIsHide(true)
+    },[isHide])
+    // const hideMenu = () => {
+    //     setIsHide(true)
+    // }
+    // const showMenu = () => {
+    //     setIsHide(false)
+    // }
+    console.log('Menu render')
 
     return (
         <div className={s.menu}>
-            {isHide
-                ? <button onClick={showMenu}>menu</button>
-                : <div className={s.menuActive}>
-                    <button onClick={hideMenu}>menu</button>
-                    <NavLink to='/restore' className={s.link} activeClassName={s.activeLink}>Forgot password</NavLink>
-                    <NavLink to='/register' className={s.link} activeClassName={s.activeLink}>Register</NavLink>
-                    {
-                        authMe
-                            ? <NavLink onClick={logOut} to='/login' className={s.link} activeClassName={s.activeLink}>
-                                Log out
-                            </NavLink>
-                            : <NavLink to='/login' className={s.link} activeClassName={s.activeLink}>Login</NavLink>
-                    }
-                    <NavLink to='/profile' className={s.link} activeClassName={s.activeLink}>Profile</NavLink>
-                </div>
-            }
+
+            <button onClick={showHideMenuTrigger}>menu</button>
+            {isHide && <div className={s.menuActive}>
+                <NavLink to='/restore' className={s.link} activeClassName={s.activeLink}>Forgot password</NavLink>
+                <NavLink to='/register' className={s.link} activeClassName={s.activeLink}>Register</NavLink>
+                {authMe
+                    ? <NavLink onClick={logOut} to='/login' className={s.link} activeClassName={s.activeLink}>
+                        Log out
+                    </NavLink>
+                    : <NavLink to='/login' className={s.link} activeClassName={s.activeLink}>Login</NavLink>}
+                <NavLink to='/profile' className={s.link} activeClassName={s.activeLink}>Profile</NavLink>
+            </div>}
+
+            {/*{isHide*/}
+            {/*    ? <button onClick={showMenu}>menu</button>*/}
+            {/*    : <div className={s.menuActive}>*/}
+            {/*        <button onClick={hideMenu}>menu</button>*/}
+            {/*        <NavLink to='/restore' className={s.link} activeClassName={s.activeLink}>Forgot password</NavLink>*/}
+            {/*        <NavLink to='/register' className={s.link} activeClassName={s.activeLink}>Register</NavLink>*/}
+            {/*        {*/}
+            {/*            authMe*/}
+            {/*                ? <NavLink onClick={logOut} to='/login' className={s.link} activeClassName={s.activeLink}>*/}
+            {/*                    Log out*/}
+            {/*                </NavLink>*/}
+            {/*                : <NavLink to='/login' className={s.link} activeClassName={s.activeLink}>Login</NavLink>*/}
+            {/*        }*/}
+            {/*        <NavLink to='/profile' className={s.link} activeClassName={s.activeLink}>Profile</NavLink>*/}
+            {/*    </div>*/}
+            {/*}*/}
         </div>
 
     )
-}
+})
 
 export default Menu
--- src/main/authGroup/LoginPage/LoginForm.tsx
+++ src/main/authGroup/LoginPage/LoginForm.tsx
@@ -5,11 +5,12 @@ import Checkbox from "../../../Components/Checkbox/Checkbox";
 import {useDispatch, useSelector} from "react-redux";
 import {StateType} from "../../../store/redux-store";
 import {useForm} from "react-hook-form";
-import {ErrorInType, InitialLoginReducerState, setLogin} from "../../../store/LoginReducer";
+import {ErrorInType, setLogin} from "../../../store/LoginReducer";
 import * as yup from "yup";
 import {yupResolver} from '@hookform/resolvers/yup';
 import s from './Login.module.scss'
 import {Redirect} from 'react-router-dom';
+import Preloader from "../../../Components/Preloader/Preloader";
 
 type LoginFormType = {
     className?: string
@@ -37,43 +38,46 @@ const LoginForm = React.memo((props: LoginFormType) => {
     const authMe = useSelector<StateType, boolean>(state => state.login.authMe)
     const error = useSelector<StateType, string>(state => state.login.error)
     const errorIn = useSelector<StateType, ErrorInType | undefined>(state => state.login.errorIn)
-    const isFetch = useSelector<StateType, boolean>(state => state.login.isFetching)
-
+    // const isFetch = useSelector<StateType, boolean>(state => state.login.isFetching)
+    const disable = useSelector<StateType, boolean>(state => state.disable.disable)
+    const loading = useSelector<StateType, boolean>(state => state.loading.isLoading)
 
     const onSubmit = useCallback(function (data: FormType) {
         dispatch(setLogin(data.login, data.password, data.rememberMe))
         reset()
     }, [authMe])
 
-    console.log(authMe)
+    console.log('LoginForm (authMe): ', authMe)
     if (authMe) return <Redirect to={'/profile'}/>
-
     return (
-        <div className={props.className}>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <Input
-                    label={'Login'}
-                    error={error}
-                    name={'login'}
-                    register={register}
-                    disable={isFetch}
-                />
-                {errors.login && <span className={s.errorField}>{errors.login.message}</span>}
-                {errorIn === "email" && <span className={s.errorField}>{error}</span>}
-                <Input
-                    type={'password'}
-                    label={'Password'}
-                    error={error}
-                    name={'password'}
-                    register={register}
-                    disable={isFetch}
-                />
-                {errors.password && <span className={s.errorField}>{errors.password.message}</span>}
-                {errorIn === "password" && <span className={s.errorField}>{error}</span>}
-                <Checkbox name={'rememberMe'} register={register} disable={isFetch}/>
-                <Button title={'login'} disable={isFetch}/>
-            </form>
-        </div>
+        <>
+            {loading && <Preloader/>}
+            <div className={props.className}>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <Input
+                        label={'Login'}
+                        error={error}
+                        name={'login'}
+                        register={register}
+                        disable={disable}
+                    />
+                    {errors.login && <span className={s.errorField}>{errors.login.message}</span>}
+                    {errorIn === "email" && <span className={s.errorField}>{error}</span>}
+                    <Input
+                        type={'password'}
+                        label={'Password'}
+                        error={error}
+                        name={'password'}
+                        register={register}
+                        disable={disable}
+                    />
+                    {errors.password && <span className={s.errorField}>{errors.password.message}</span>}
+                    {errorIn === "password" && <span className={s.errorField}>{error}</span>}
+                    <Checkbox name={'rememberMe'} register={register} disable={disable}/>
+                    <Button title={'login'} disable={disable}/>
+                </form>
+            </div>
+        </>
     )
 })
 
