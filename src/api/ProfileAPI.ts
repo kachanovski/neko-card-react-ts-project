933a046860bd6e81efde1ec32b795dff6d0350c6
kachanovski <58528758+kachanovski@users.noreply.github.com>
2020-10-23 19:13:07 +0300

    fix url

--- src/api/ProfileAPI.ts
+++ src/api/ProfileAPI.ts
@@ -4,7 +4,7 @@ import { ProfileDataType } from "../store/ProfileReducer";
 
 const instance = axios.create({
     withCredentials:true,
-    baseURL:"http://localhost:7542/2.0/"
+    baseURL: "https://neko-back.herokuapp.com/2.0"
 })
 
 export const ProfileAPI = {
